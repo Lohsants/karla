73d275ef67c8d392fd9cbeb2b7c3f44afed00de8
LOH <karla.nascimento.santos@escola.pr.gov.br>
2024-10-23 20:33:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ //Armazenar oss dados
+ let dados = [];
+
+ // Gerar ID aleatório com 5 dígitos
+ function gerarld(){
+   return Math.floor(10000 + Math.random() * 90000);
+ } 
+
+  //Adicionar dados à tabela
+  document.getElementByld('form').addEventListener9 ('submit, function(event) {
+    Event.preventDefault
+
+
+
+
+
+
+
+
+
+}
+
